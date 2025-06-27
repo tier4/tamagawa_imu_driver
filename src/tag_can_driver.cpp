cb1918a08f3a3fe7216686a5633ae69dad354f88
Amadeusz Szymko <amadeuszszymko@gmail.com>
2025-06-27 14:23:37 +0900

    feat: update rate bounds (#12)
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>

--- src/tag_can_driver.cpp
+++ src/tag_can_driver.cpp
@@ -110,17 +110,17 @@ int main(int argc, char ** argv)
   imu_frame_id = node->declare_parameter<std::string>("imu_frame_id", "imu");
   auto frequency_reference = node->declare_parameter<double>("frequency_reference", 200.0);
   auto ok_min_freq = node->declare_parameter<double>(
-    "diagnostics.rate_bound_status.frequency_ok.min_hz", 175.0);
+    "diagnostics.rate_bound_status.frequency_ok.min_hz", 100.0);
   auto ok_max_freq = node->declare_parameter<double>(
-    "diagnostics.rate_bound_status.frequency_ok.max_hz", 225.0);
+    "diagnostics.rate_bound_status.frequency_ok.max_hz", 10000.0);
   auto warn_min_freq = node->declare_parameter<double>(
-    "diagnostics.rate_bound_status.frequency_warn.min_hz", 150.0);
+    "diagnostics.rate_bound_status.frequency_warn.min_hz", 50.0);
   auto warn_max_freq = node->declare_parameter<double>(
-    "diagnostics.rate_bound_status.frequency_warn.max_hz", 250.0);
+    "diagnostics.rate_bound_status.frequency_warn.max_hz", 100000.0);
   node->declare_parameter<bool>("diagnostic_updater.use_fqn", true);  // read by diagnostic updater
   rate_bound_status = std::make_unique<custom_diagnostic_tasks::RateBoundStatus>(
     node.get(), custom_diagnostic_tasks::RateBoundStatusParam(ok_min_freq, ok_max_freq),
-    custom_diagnostic_tasks::RateBoundStatusParam(warn_min_freq, warn_max_freq), 10, false);
+    custom_diagnostic_tasks::RateBoundStatusParam(warn_min_freq, warn_max_freq), 2, false);
   diag_updater = std::make_unique<diagnostic_updater::Updater>(node);
   diag_updater->setHardwareID(imu_frame_id);
   diag_updater->setPeriod(1.0 / frequency_reference);
